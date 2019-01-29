d6a07447b22ac7b0d25d2b973efe2d35ce780bf8
Quentin <quentin.angeli@live.com>
2019-01-29 12:23:26 +0100

    ClassMethods -> récupération des méthodes d'une classe donnée

---
+++ CodeMutation/src/main/java/main/java/ClassMethods.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class ClassMethods {
+
+    private String[] methods;
+
+    public String[] getMethods() {
+        return methods;
+    }
+
+    public void setMethods(String[] methods) {
+        this.methods = methods;
+    }
+
+    public void getMethodsFromClass(String path,String className){
+
+    }
+}
