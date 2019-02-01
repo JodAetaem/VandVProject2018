83eb03c02e3041dea1086d951085bcaac62a42b8
JodAetaem <clarzi@gmail.com>
2019-02-01 09:51:42 +0100

    Corrections (non fonctionnel a ce point)

---
+++ CodeMutation/src/test/java/TestTest.java
@@ -0,0 +1,20 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+
+    @Before
+    public void init(){}
+
+    @After
+    public void after(){
+    }
+
+    @Test
+    public void shouldAddCorrectly(){
+        assertTrue(true);
+    }
+}
