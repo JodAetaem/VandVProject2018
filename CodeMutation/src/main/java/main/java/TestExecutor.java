0601d43eb69e69cbb5d9492c946527cf53b236d9
JodAetaem <clarzi@gmail.com>
2019-02-04 16:26:33 +0100

    execution a distance

--- CodeMutation/src/main/java/main/java/TestExecutor.java
+++ CodeMutation/src/main/java/main/java/TestExecutor.java
@@ -1,10 +1,10 @@
 package main.java;
 
-import org.junit.runner.*;
+import org.junit.runner.Description;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
-import org.junit.runner.notification.RunNotifier;
-import org.junit.runner.notification.StoppedByUserException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,43 +12,19 @@ import java.net.URLClassLoader;
 
 public class TestExecutor {
 
-    public void executeTestFromADistance() throws ClassNotFoundException, MalformedURLException {
-
-//        URL classUrl;
-//        classUrl = new URL("file:///home/istic-larzilliere/Documents/VV/Project2019/SourceCode/target/test-classes/");
-//        URL[] classUrls = { classUrl };
-//        URLClassLoader ucl = new URLClassLoader(classUrls);
-//        Class c = ucl.loadClass("MathOperationTest");
-//
-//        Class<?>[] classes = {c};
-//        Request request = Request.classes(new Computer(), classes);
-//        Runner runner = request.getRunner();
-//        Result result = new Result();
-//        RunNotifier notifier = new RunNotifier();
-//       // RequestStopListener listener = new RequestStopListener(notifier);
-//
-//
-//        try {
-//            runner.run(notifier);
-//        }
-//        catch(StoppedByUserException exc) {
-//            System.out.println("Process stopped");
-//        }
-//        System.out.println("Did all test passed? " + result.wasSuccessful());
-//        System.out.print("Tests executed: " + result.getRunCount());
-
+    public void executeTestFromADistance(String pathToTestClasses, String pathToImplementation) throws ClassNotFoundException, MalformedURLException {
         JUnitCore core = new JUnitCore();
 
-        core.addListener( new RunListener() {
+        core.addListener(new RunListener() {
 
             @Override
             public void testRunStarted(Description description) throws Exception {
-                System.out.println("RUN STARTED");
+                System.out.println("DEBUT DES TESTS");
             }
 
             @Override
             public void testStarted(Description description) throws Exception {
-                System.out.println("TEST STARTED: " +  description.getDisplayName());
+                //System.out.println("TEST STARTED: " + description.getDisplayName());
             }
 
             @Override
@@ -58,12 +34,12 @@ public class TestExecutor {
 
             @Override
             public void testIgnored(Description description) throws Exception {
-                System.out.println("TEST IGNORED: " + description.getDisplayName());
+                //System.out.println("TEST IGNORED: " + description.getDisplayName());
             }
 
             @Override
             public void testFinished(Description description) throws Exception {
-                System.out.println("TEST FINISHED: " + description.getDisplayName());
+                //System.out.println("TEST FINISHED: " + description.getDisplayName());
             }
 
 
@@ -75,16 +51,15 @@ public class TestExecutor {
                 System.out.println(String.format("| RUN: %d", result.getRunCount()));
             }
 
-        } );
+        });
 
-        URL classUrl;
-        classUrl = new URL("file:///home/istic-larzilliere/Documents/VV/Project2019/SourceCode/target/test-classes/");
-        URL[] classUrls = { classUrl };
-        URLClassLoader ucl = new URLClassLoader(classUrls);
+        URL classUrl = new URL("file://" + pathToTestClasses);
+        URL classUrl2 = new URL("file://" + pathToImplementation);
+        URL[] classUrls = {classUrl, classUrl2};
+        URLClassLoader ucl = new URLClassLoader(classUrls, getClass().getClassLoader());
         Class c = ucl.loadClass("MathOperationTest");
 
-
-        core.run(c.getClass());
+        core.run(c);
 
     }
 
