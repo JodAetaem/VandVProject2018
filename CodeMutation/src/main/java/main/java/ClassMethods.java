a24730efaf21feb2afe476af8a4133c8fb84c3da
Quentin <quentin.angeli@live.com>
2019-02-05 12:47:45 +0100

    ajout de classmethod

--- CodeMutation/src/main/java/main/java/ClassMethods.java
+++ CodeMutation/src/main/java/main/java/ClassMethods.java
@@ -1,18 +1,36 @@
 package main.java;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassMethods {
 
-    private String[] methods;
+    private List<String> methods = new ArrayList<String>();
 
-    public String[] getMethods() {
+    public List<String> getMethods() {
         return methods;
     }
 
-    public void setMethods(String[] methods) {
+    public void setMethods(List<String> methods) {
         this.methods = methods;
     }
 
-    public void getMethodsFromClass(String path,String className){
+    public void getMethodsFromClass(String pathname,String className) throws NotFoundException {
+
+        methods.clear();
+        ClassPool pool = ClassPool.getDefault();
+        pool.appendClassPath(pathname);
+
+        CtClass functions = pool.get(className);
+
+        for(CtMethod method : functions.getDeclaredMethods()) {
+            this.methods.add(method.getName());
+        }
 
     }
 }
